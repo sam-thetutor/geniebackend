bbd216326ea5c654255486a88b9d71bf5cb4f085
sam-thetutor <atwebembeiresamuel2020@gmail.com>
2025-04-27 13:34:03 +0300

    s

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ process.on('SIGINT', () => {
 });
 
  telegramBot.start();
-//discordBot.start();
+discordBot.start();
 twitterBot.start();
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
